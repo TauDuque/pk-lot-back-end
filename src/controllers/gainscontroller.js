7aa0bf8d04cbfded62100b6bf2c4e4fedb90ef9c
TauDuque <hectauduque@gmail.com>
2021-07-24 23:38:27 -0300

    gains controllers

---
+++ src/controllers/gainscontroller.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const Gains = require("../models/Gains");
+
+const getAllGain = (req, res) => {
+  const data = Gains.findAll()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(500).json("Houve um erro na requisição..."));
+};
+
+const getGainId = (req, res) => {
+  const { id } = req.params;
+  const data = Gains.findAll({ where: { id: id } })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(500).json("Houve um erro na requisição..."));
+};
+
+const postGain = (req, res) => {
+  const { veiculos, valor, vagas_livres } = req.body;
+  const data = Gains.create({ veiculos, valor, vagas_livres })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) =>
+      res.status(500).json("Houve um erro durante o cadastro...")
+    );
+};
+
+const updateGain = (req, res) => {
+  const { id, veiculos, valor, vagas_livres } = req.body;
+  const data = Gains.update(
+    { veiculos, valor, vagas_livres },
+    { where: { id: id } }
+  )
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) =>
+      res.status(500).json("Houve um erro durante a atualização...")
+    );
+};
+
+const deleteGain = (req, res) => {
+  const { id } = req.params;
+  const data = Gains.destroy({ where: { id: id } })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => res.status(500).json("Houve um erro..."));
+};
+
+module.exports = {
+  getGainId: getGainId,
+  getAllGain: getAllGain,
+  postGain: postGain,
+  updateGain: updateGain,
+  deleteGain: deleteGain,
+};
--- src/routes.js
+++ src/routes.js
@@ -2,46 +2,32 @@ const express = require("express");
 const Gains = require("./models/Gains");
 const Veiculos = require("./models/Veiculos");
 
+const gain = require("./controllers/gainscontroller");
+
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("Working...");
 });
 
-router.get("/gains", async (req, res) => {
-  try {
-    const data = await Gains.findAll();
-    return res.json(data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ err: "Houve um erro na requisição..." });
-  }
+router.get("/gains", (req, res) => {
+  gain.getAllGain(req, res);
 });
 
-router.get("/gains/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const data = await Gains.findAll({ where: { id: id } });
-    return res.json(data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ err: "Houve um erro na requisição..." });
-  }
+router.get("/gains/:id", (req, res) => {
+  gain.getGainId(req, res);
 });
 
-router.post("/gains", async (req, res) => {
-  try {
-    const { veiculos, valor, vagas_livres } = req.body;
-    const data = await Gains.create({
-      veiculos,
-      valor,
-      vagas_livres,
-    });
-    return res.json(data);
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({ err: "Houve um erro durante o cadastro..." });
-  }
+router.post("/gains", (req, res) => {
+  gain.postGain(req, res);
+});
+
+router.put("/gains", (req, res) => {
+  gain.updateGain(req, res);
+});
+
+router.delete("/gains/:id", (req, res) => {
+  gain.deleteGain(req, res);
 });
 
 module.exports = router;
