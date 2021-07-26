694cf6d4ae100327535a060070e1e04f9c062ade
TauDuque <hectauduque@gmail.com>
2021-07-26 17:30:10 -0300

    final heroku issue

--- src/config/database.js
+++ src/config/database.js
@@ -3,8 +3,8 @@ module.exports = {
   host: "us-cdbr-east-04.cleardb.com",
   username: "b726ee680a1ee9",
   password: "f09e215f",
-  port: 3306,
-  database: "pk-lot",
+  port: "3306",
+  database: "heroku_78e4989f6759e87",
   define: {
     timestamps: false,
     underscored: true,
