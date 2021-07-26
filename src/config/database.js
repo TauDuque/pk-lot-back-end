0001618906370d83628b679418c0f779cb896663
TauDuque <hectauduque@gmail.com>
2021-07-26 17:27:18 -0300

    keys witj clearmysql from heroku

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
   dialect: "mysql",
-  host: "localhost",
-  username: "root",
-  password: "",
+  host: "us-cdbr-east-04.cleardb.com",
+  username: "b726ee680a1ee9",
+  password: "f09e215f",
   port: 3306,
   database: "pk-lot",
   define: {
